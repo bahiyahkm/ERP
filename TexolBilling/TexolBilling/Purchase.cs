6a4a47b6c68f67cb7f20bfc775cc70c9c1dead32
Bahiyah <bahiyahperumbadiyil6@gmail.com>
2019-05-14 09:32:04 +0530

    test msg

--- TexolBilling/TexolBilling/Purchase.cs
+++ TexolBilling/TexolBilling/Purchase.cs
@@ -74,7 +74,7 @@ namespace TexolBilling
         private void Purchase_Load(object sender, EventArgs e)
         {
             CommonFunctions objcmn = new CommonFunctions();
-            txtPurchaseTNo.Text = objcmn.GenerateRandomNo(); //test
+            txtPurchaseTNo.Text = objcmn.GenerateRandomNo(); //testtttt
             
         }
 
